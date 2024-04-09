66e8272fae69853bd73b5654c15e1de20d288838
jorgegdev <polloandres82@gmail.com>
2024-04-09 17:45:25 +1200

    Shader Galaxy on three js

---
+++ src/shaders/galaxy/fragment.glsl
@@ -0,0 +1,43 @@
+varying vec3 vColor;
+
+
+void main()
+            {
+
+                //BECOME DISK
+
+                // float strength = distance(gl_PointCoord, vec2(0.5));
+                // strength = step(0.5, strength);
+                // strength = 1.0 - strength;
+
+
+                //DIFFUSE PATTERN
+
+                // float strength = distance(gl_PointCoord, vec2(0.5));
+                // strength *= 2.0;
+                // strength = 1.0 - strength;
+
+                //LIGHT POINT PATTERN
+
+                float strength = distance(gl_PointCoord, vec2(0.5));
+                strength = 1.0 - strength;
+                strength = pow(strength, 10.0);
+
+
+
+
+
+
+
+                //FINAL COLOR
+
+                vec3 color = mix(vec3(0.0), vColor, strength);
+
+
+
+                gl_FragColor = vec4(color, 1.0);
+                #include <colorspace_fragment>
+            }
+
+
+
